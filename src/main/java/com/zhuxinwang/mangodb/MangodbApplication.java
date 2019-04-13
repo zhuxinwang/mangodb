2eb620a8293c73dc708989a37d1cf7cb0b2c8f39
zhuxinwang <zhuxinwang@aliyun.com>
2019-04-13 09:20:40 +0800

    初始提交

---
+++ src/main/java/com/zhuxinwang/mangodb/MangodbApplication.java
@@ -0,0 +1,13 @@
+package com.zhuxinwang.mangodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhuxinwang/mangodb/MangodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhuxinwang.mangodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MangodbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
